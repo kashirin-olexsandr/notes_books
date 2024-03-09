78623138331dfd380b04974d9648cdbc0a3f15a0
Alex <kashirin.olexsandr91@gmail.com>
2024-03-09 14:02:53 +0200

    fix lang

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  *
  * Розмітка картки задачі
  * <li class="task-list-item">
- *     <button class="task-list-item-btn">Удалить</button>
+ *     <button class="task-list-item-btn">Видалити</button>
  *     <h3>Заголовок</h3>
  *     <p>Текст</p>
  * </li>
